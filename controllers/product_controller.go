81e1b8f20d1a9051b02ae406c46ecabe5d66e2ed
fgeanlopes <f.geanlopes@gmail.com>
2026-01-30 11:13:08 -0300

    Enhance product creation with error handling and add .air.toml for live reload configuration

--- controllers/product_controller.go
+++ controllers/product_controller.go
@@ -1,18 +1,26 @@
 package controllers
 
 import (
+	"go-pratica/database"
 	"go-pratica/models"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateProduct(c *gin.Context) {
 	var product models.Product
-
 	if err := c.ShouldBindJSON(&product); err != nil {
-		c.JSON(400, gin.H{"error": "Dados inválidos:" + err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Dados inválidos:" + err.Error()})
 		return
 	}
+
+	if err := database.DB.Create(&product).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao criar produto:" + err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, product)
 
 }
 
