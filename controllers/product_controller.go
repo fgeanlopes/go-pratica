2b6c541661c36644ce60f1ed6a41ea176fa14d4c
fgeanlopes <f.geanlopes@gmail.com>
2026-02-01 10:04:16 -0300

    Implement product update logic with error handling and validation

--- controllers/product_controller.go
+++ controllers/product_controller.go
@@ -5,6 +5,7 @@ import (
 	"go-pratica/dto"
 	"go-pratica/models"
 	"net/http"
+	"strconv"
 
 	"github.com/asaskevich/govalidator"
 	"github.com/gin-gonic/gin"
@@ -46,16 +47,71 @@ func CreateProduct(c *gin.Context) {
 }
 
 func UpdateProduct(c *gin.Context) {
-	// id := c.Param("id")
+	id := c.Param("id")
 
-	// ProductID, err := strconv.ParseUint(id, 10, 32)
+	productID, err := strconv.ParseUint(id, 10, 32)
 
-	// if err != nil {
-	// 	c.JSON(http.StatusBadRequest, gin.H{"error": "ID inválido"})
-	// 	return
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID inválido"})
+		return
+	}
+
+	var update dto.ProductUpdate
+
+	if err := c.ShouldBindJSON(&update); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Dados inválidos:" + err.Error()})
+		return
+	}
+
+	// Validação usando govalidator
+	if _, err := govalidator.ValidateStruct(update); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var productUpdate models.Product
+
+	if err := database.DB.First(&productUpdate, uint(productID)).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"erro": "Produto não encontrado"})
+		return
+	}
+
+	if update.Name != nil {
+		productUpdate.Name = *update.Name
+	}
+
+	if update.Price != nil {
+		productUpdate.Price = *update.Price
+	}
+
+	if err := database.DB.Save(&productUpdate).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao atualizar produto:" + err.Error()})
+		return
+	}
+
+	response := dto.ProductResponse{
+		ID:        productUpdate.ID,
+		Name:      productUpdate.Name,
+		Price:     productUpdate.Price,
+		CreatedAt: productUpdate.CreatedAt,
+		UpdatedAt: productUpdate.UpdatedAt,
+	}
+
+	c.JSON(http.StatusOK, response)
+
+	//Buscar o produto
+	//Atualizar o model
+	//Salvar o model no banco
+
+	// var productUpdate models.Product
+
+	// if update.Name != nil {
+	// 	update.Name = *&update.Name
 	// }
 
-	// var product models.Product
+	// if update.Price != nil {
+	// 	update.Price = *&update.Price
+	// }
 
 }
 
